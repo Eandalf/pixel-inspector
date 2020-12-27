523866686a78e73f0266cf235d64c5fc9d193378
Eandalf <clinah@connect.ust.hk>
2020-12-27 23:08:27 +0800

    Make the inspector into a class

--- inspector.py
+++ inspector.py
@@ -3,11 +3,17 @@ import os
 import numpy as np
 import matplotlib.image as mpimg
 
-def inspector(path, x, y):
-    img = mpimg.imread(path)
-    pixel = 255 * img[x, y, :]
-
-    return pixel.astype(int)
+class PixelInspector:
+    def __init__(self, path):
+        try:
+            self.img = mpimg.imread(path)
+        except FileNotFoundError:
+            print("No such file or directory.")
+            self.img = np.zeros((100,100,3))
+    
+    def inspect(self, x,y):
+        pixel = 255 * self.img[x, y, :]
+        return pixel.astype(int)
 
 path = os.path.join('./demo', 'stinkbug.png')
 # dir_path = '' # A linux style absolute path.
@@ -16,6 +22,7 @@ path = os.path.join('./demo', 'stinkbug.png')
 # print(path)
 
 try:
-    print(inspector(path, 300, 300))
+    pi = PixelInspector(path)
+    print(pi.inspect(300, 300))
 except IndexError:
     print("Index is out of bound. Please check the image size.")
